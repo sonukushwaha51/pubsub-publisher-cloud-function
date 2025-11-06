668df4eb7808bd3cffc42ab6741af4f6ef6d6df1
sonukushwaha51 <sonukushwaha51@gmail.com>
2025-11-06 23:58:59 +0530

    added pubsub message pojo

--- src/main/java/com/labs/cloud/function/pusub/PubsubTriggerCloudFunction.java
+++ src/main/java/com/labs/cloud/function/pusub/PubsubTriggerCloudFunction.java
@@ -5,9 +5,9 @@ import com.google.cloud.functions.BackgroundFunction;
 import com.google.cloud.functions.Context;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.pubsub.v1.PubsubMessage;
 import com.labs.cloud.function.pusub.configuration.ConfigurationModule;
 import com.labs.cloud.function.pusub.event.Event;
+import com.labs.cloud.function.pusub.event.PubsubMessage;
 import com.labs.cloud.function.pusub.properties.PropertiesModule;
 import com.labs.cloud.function.pusub.schedule.SchedulingModule;
 import com.labs.cloud.function.pusub.schedule.SchedulingService;
@@ -35,7 +35,7 @@ public class PubsubTriggerCloudFunction implements BackgroundFunction<PubsubMess
     @Override
     public void accept(PubsubMessage pubsubMessage, Context context) throws Exception {
 
-        String eventPayload = pubsubMessage.getData().toString(StandardCharsets.UTF_8);
+        String eventPayload = pubsubMessage.getData();
         Event event = objectMapper.readValue(eventPayload, Event.class);
         SchedulingService schedulingService = injector.getInstance(SchedulingService.class);
         log.info("Proceeding with creating cloud task for event: {}", eventPayload);
